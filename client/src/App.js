4ea7bac6970c28900d63e21840a7123f38330740
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2021-12-17 11:00:22 +0100

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Login from './components/auth/LoginForm';
-import AuthContextProvider from './contexts/AuthContext';
 import Navbar from './components/navbar/Navbar';
-// import Landing from './components/layout/Landing';
 
 import User from './routers/user.router';
 import Upload from './routers/upload.router';
@@ -12,16 +10,13 @@ import Upload from './routers/upload.router';
 
 function App() {
   return (
-    <AuthContextProvider>
       <Router>
          <Routes>
-            <Route path="/" element={<Login/>} />
-            {/* <Route path="/" element={<Landing/>} /> */}
-            {/* <Route path="/users" element={<User/>} />
-            <Route path="/upload" element={<Upload/>} /> */}
+            <Route path="/login" element={<Login/>} />
+            <Route path="/user" element={<User/>} />
+            <Route path="/upload" element={<Upload/>} />
          </Routes>
       </Router>
-    </AuthContextProvider>
   );
 }
 
--- client/src/contexts/constants.js
+++
@@ -1,1 +0,0 @@
-export const LOCAL_STORAGE_TOKEN_NAME = "flickr-token";
